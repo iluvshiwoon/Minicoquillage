ddc881c01a57ab9b77c29ca5ea4ce2c7d6da3a3e
iluvshiwoon <sntcillian@gmail.com>
2024-05-22 15:42:25 +0200

    Wait for matching quote on input

--- Tokenizer/Tokenizer.c
+++ Tokenizer/Tokenizer.c
@@ -1,19 +1,102 @@
 #include "../Minicoquillage.h"
 
+int check_syntax(char * line)
+{
+    static size_t single_quote;
+    static size_t double_quote;
+
+    while(*line)
+    {
+        if (*line == '\'')
+            single_quote++;
+        else if (*line == '\"')
+            double_quote++;
+        line++;
+    }
+    return (single_quote % 2 == 0 && double_quote % 2 == 0);
+}
+
+char * concat_input(t_double_link_list * list)
+{
+    t_double_link_node * node;
+    t_double_link_node * temp_node;
+    size_t i;
+    char * dst;
+    char * temp;
+    size_t l1;
+    size_t l2;
+
+    node = list->first_node;
+    i = 0;
+    while (node)
+    {
+        node = node->next;
+        i++;
+    }
+    node = list->first_node;
+
+
+    if (i == 1)
+        dst = node->data;
+    else if (i >= 2)
+    {
+        dst = ft_strjoin((char *)node->data, (char *)node->next->data);
+        i--;
+        node = node->next->next;
+    }
+    while (i != 1)
+    {
+        temp = ft_strjoin(dst,(char *)node->data);
+        free(dst);
+        dst = temp;
+        node = node->next;
+        i--;
+    }
+    return (dst);
+}
+
+char * get_line()
+{
+    char * prompt; 
+    char * line;
+    t_double_link_list * list;
+    t_double_link_node * node;
+    char * final;
+    
+    list = malloc(sizeof(*list));
+    init_list(list);
+    node = malloc(sizeof(*node));
+    prompt = build_prompt();
+    line = readline(prompt);
+    node->data = line;
+    list->pf_insert_end(list, node);
+    while (!check_syntax(line))
+    {
+        node = malloc(sizeof(*node));
+        line = readline("· ");
+        node->data = line;
+        list->pf_insert_end(list, node);
+    }    
+    final = concat_input(list); 
+    if (final && *final)
+       add_history(final);
+    if (!list->first_node->next)
+        final = ft_strdup(final);
+    dl_free_list(list);
+    return (free(prompt),final);
+}
+
 t_double_link_list ** tokenizer(void)
 {
-    char * line;
     char ** multiline;
-    char * prompt;
+    char * line;
 
     // line = "echo 'hello' ; echo 'world' ; ff ; bot diff; aaargh";
     
-    prompt = build_prompt();
-    line = readline(prompt);
-    if (line && *line)
-        add_history(line);
+    line = get_line();
     multiline = ft_mysplit(line, "\n;");
-    return (free(prompt),create_tokens_lists(multiline));
+    free(line);
+    return (create_tokens_lists(multiline));
 }
 
 int main (void)
