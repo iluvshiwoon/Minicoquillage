11bcedf75bba49d90ed5aa1b1d24d6c152c4fee3
iluvshiwoon <sntcillian@gmail.com>
2024-05-22 13:24:33 +0200

    Cleanup

--- Tokenizer/Tokenizer.h
+++ Tokenizer/Tokenizer.h
@@ -1,27 +1,35 @@
 #ifndef TOKENIZER_H
 # define TOKENIZER_H
 # include "../Minicoquillage.h"
+
 typedef enum e_token_type {
     COMMAND, // relative or absolute if needed
-    LITTERAL, //need to be concatened with something
-    EXPANSION,
     ARG,
+    ARG_LITTERAL,
     PIPE,
     REDIRECTION,
     HERE_DOC,
-    DOUBLE_QUOTE,
-    SINGLE_QUOTE,
-    VARIABLE,
     OPTION,
     AND,
     OR,
-    PARENTHESIS, // Close and open if needed
+    OPEN_PARENTHESIS,
+    CLOSE_PARENTHESIS,
 } t_token_type;
 
 typedef struct s_token {
     int type;
     char * value;
 } t_token;
+
+typedef struct s_status {
+    int index;
+    int exist;
+} t_status;
+ 
+typedef struct s_matching {
+    t_status open;    
+    t_status close;
+} t_matching;
 
 // Build_lists.c
 t_double_link_list ** fill_tokens_lists(char ** multiline, char *** tokens, t_double_link_list ** tokens_lists);
@@ -33,8 +41,12 @@ t_double_link_list ** tokenizer(void);
 
 // Free.c
 void dl_free_list(t_double_link_list * tokens_list);
-void free_all(char ** multiline, t_double_link_list ** tokens_list, char *** tokens);
+void free_all(char ** multiline, t_double_link_list ** tokens_list);
 void free_previous_tokens(size_t i, char *** tokens);
+
+// Prompt.c
+char * build_prompt();
+char * last_ocur (char * string , char c);
 
 // Utils.c
 void print_tokens_list(t_double_link_list ** tokens_lists);
