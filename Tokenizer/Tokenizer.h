d3a1e32a04e9a8c496a0e345d0d0287f5417b80a
iluvshiwoon <sntcillian@gmail.com>
2024-05-22 11:19:12 +0200

    Cleanup

--- Tokenizer/Tokenizer.h
+++ Tokenizer/Tokenizer.h
@@ -3,6 +3,8 @@
 # include "../Minicoquillage.h"
 typedef enum e_token_type {
     COMMAND, // relative or absolute if needed
+    LITTERAL, //need to be concatened with something
+    EXPANSION,
     ARG,
     PIPE,
     REDIRECTION,
@@ -31,7 +33,7 @@ t_double_link_list ** tokenizer(void);
 
 // Free.c
 void dl_free_list(t_double_link_list * tokens_list);
-static void free_all(char ** multiline, t_double_link_list ** tokens_list, char *** tokens);
+void free_all(char ** multiline, t_double_link_list ** tokens_list, char *** tokens);
 void free_previous_tokens(size_t i, char *** tokens);
 
 // Utils.c
