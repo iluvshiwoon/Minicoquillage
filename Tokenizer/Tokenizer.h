86db757a98c29f229a0534795fffe04a401c76e7
iluvshiwoon <sntcillian@gmail.com>
2024-05-22 13:25:48 +0200

    Cleanup

--- Tokenizer/Tokenizer.h
+++ Tokenizer/Tokenizer.h
@@ -41,7 +41,8 @@ t_double_link_list ** tokenizer(void);
 
 // Free.c
 void dl_free_list(t_double_link_list * tokens_list);
-void free_all(char ** multiline, t_double_link_list ** tokens_list);
+// void free_all(char ** multiline, t_double_link_list ** tokens_list);
+void free_all(char ** multiline, t_double_link_list ** tokens_list, char *** tokens);
 void free_previous_tokens(size_t i, char *** tokens);
 
 // Prompt.c
