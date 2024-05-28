cb3c7e402d78f6f468dd668d73754a8f2ef61243
iluvshiwoon <sntcillian@gmail.com>
2024-05-28 22:33:33 +0000

    split token

--- Tokenizer/Tokenizer.h
+++ Tokenizer/Tokenizer.h
@@ -21,16 +21,6 @@ typedef struct s_token {
     char * value;
 } t_token;
 
-typedef struct s_status {
-    int index;
-    int exist;
-} t_status;
- 
-typedef struct s_matching {
-    t_status open;    
-    t_status close;
-} t_matching;
-
 // Build_lists.c
 t_double_link_list ** fill_tokens_lists(char ** multiline, char *** tokens, t_double_link_list ** tokens_lists);
 t_double_link_list ** init_tokens_lists (char ** multiline, char *** tokens, t_double_link_list ** tokens_lists);
@@ -54,8 +44,6 @@ void print_tokens_list(t_double_link_list ** tokens_lists);
 void print_list(t_double_link_list * tokens_lists);
 
 // try 
-char * expand(char * line, size_t index, size_t j);
-size_t expand_double_quote(char * line, size_t i, size_t j, t_double_link_list * list);
 t_double_link_list * create_tokens(char * line);
 
 #endif
--- Tokenizer/Tokens.c
+++ Tokenizer/Tokens.c
@@ -1,119 +1,63 @@
 #include "../Minicoquillage.h"
-char * expand(char * line, size_t index, size_t j)
-{
-    char * expanded;
-    expanded = malloc(sizeof(char)*(j - index + 1));
-    ft_strlcpy(expanded, line + index + 1, j - index);
-    // printf("%s\n",expanded);
-    return (expanded);
+
+int ft_isspace(int c) {
+    return (c == ' ' || c == '\t' || c == '\n' || c == '\v' ||
+            c == '\f' || c == '\r');
 }
 
-size_t add_token(char * line, size_t i, size_t j, t_double_link_list * list)
+size_t skip_space(char * line, size_t index)
 {
-    char * temp;
+    if (!line[index])
+        return(index);
+    while (ft_isspace(line[index+1]))
+        ++index;
+    return (index);
+}
+
+void add_token(size_t i, size_t j, char * line, t_double_link_list * list)
+{
     t_double_link_node * node;
+    char * temp;
 
     node = malloc(sizeof(*node));
-    temp = malloc(sizeof(char)*(j-i + 1));
-    ft_strlcpy(temp, line + i, j-i +3);
+    temp = malloc(sizeof(char)*(j-i+1));
+    ft_strlcpy(temp, line + i, j-i+1);
     node->data = temp;
     list->pf_insert_end(list, node);
-    // printf("%s\n",temp);
-    return (j);
-}
-
-size_t expand_single_quote(char * line, size_t i, size_t j, t_double_link_list * list)
-{
-    size_t index;
-    char * temp;
-    char * expanded;
-    char * to_token;
-    t_double_link_node * node;
-    
-    index = i;
-    node = malloc(sizeof(*node));
-    while (line[index] != '\'' && line[index])
-        ++index;
-    temp = malloc(sizeof(char)*(index+1));
-    expanded = expand(line , index, j);
-    ft_strlcpy(temp,line,index+1);
-    to_token = ft_strjoin(temp,expanded);
-    node->data = to_token;
-    list->pf_insert_end(list, node);
-    // printf("%s\n",to_token);
-    return (free(temp), free(expanded),j);
-}
-
-
-size_t expand_double_quote(char * line, size_t i, size_t j, t_double_link_list * list)
-{
-    size_t index;
-    char * temp;
-    char * expanded;
-    char * to_token;
-    t_double_link_node * node;
-    
-    index = i;
-    node = malloc(sizeof(*node));
-    while (line[index] != '"' && line[index])
-        ++index;
-    temp = malloc(sizeof(char)*(index+1));
-    expanded = expand(line , index, j);
-    ft_strlcpy(temp,line,index+1);
-    to_token = ft_strjoin(temp,expanded);
-    node->data = to_token;
-    list->pf_insert_end(list, node);
-    // printf("%s\n",to_token);
-    return (free(temp), free(expanded),j);
 }
 
 t_double_link_list * create_tokens(char * line)
 {
     t_double_link_list * list;
     int double_quote_open;
-    int word;
     int single_quote_open;
     size_t i;
     size_t j;
 
     double_quote_open = 0;
-    word = 0;
     single_quote_open = 0;
     list = malloc(sizeof(*list));
-    i = 0;
-    j = 0;
+    j = skip_space(line, j);
+    i = j;
     init_list(list);
     while (line[j])
     {
-        if (line[j] == '"' && !double_quote_open && !single_quote_open)
+        if (line[j] == '"' && !single_quote_open && !double_quote_open)
             double_quote_open = 1;
         else if (line[j] == '\'' && !single_quote_open && !double_quote_open)
             single_quote_open = 1;
         else if (line[j] == '"' && double_quote_open)
-        {
             double_quote_open = 0;
-            i = expand_double_quote(line ,i ,j, list);
-            j = i;
-        }
         else if (line[j] == '\'' && single_quote_open)
-        {
             single_quote_open = 0;
-            i = expand_single_quote(line, i, j, list);
-            j = i;
-        }
-        else if ((line[j] == ' '/*  || line[j] == '\n' */) && !double_quote_open && !single_quote_open && !word)
+        else if (ft_isspace(line[j]) && !single_quote_open && !double_quote_open)
         {
-            word = 1;
+            add_token(i,j,line,list);
+            j = skip_space(line, j);
             i = j+1;
-        }
-        else if ((!line[j+1] || line[j + 1] == ' '/*  || line[j] == '\n' */) && !double_quote_open && !single_quote_open && word)
-        {
-            word = 0;
-            i = add_token(line, i, j, list);
-            j = i;
         }
         ++j;
     }
-
+    add_token(i, j, line, list);
     return (list);
 }
