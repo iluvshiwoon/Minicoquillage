c623f4a7a2d454500b0dd6b2df432498f5cd53a8
iluvshiwoon <sntcillian@gmail.com>
2024-09-20 18:01:12 +0200

    Update libft

--- Parser/Parser.h
+++ Parser/Parser.h
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 17:26:58 by kgriset           #+#    #+#             */
-/*   Updated: 2024/09/18 18:00:32 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/09/20 17:49:32 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,19 @@
 #ifndef MINICOQUILLAGE_H
 # include "../Minicoquillage.h"
 #endif
+
+typedef struct s_ast_node t_ast_node;
+
+struct s_ast_node{
+    t_ast_node * previous;
+    t_ast_node * left;
+    t_ast_node * right;
+};
+
+typedef struct s_ast {
+    t_ast_node * first_node; 
+    t_ast_node * node; 
+} t_ast;
 
 #endif
 
