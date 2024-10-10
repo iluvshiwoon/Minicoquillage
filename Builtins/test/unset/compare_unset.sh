4c09332bc4cd262db63256d4ff67bc958318f9f7
Bedelo Sunda <bsunda@made-f0Dr6s1.clusters.42paris.fr>
2024-10-10 13:30:29 +0200

    ft_unset testeur

--- Builtins/test/unset/compare_unset.sh
+++ Builtins/test/unset/compare_unset.sh
@@ -4,55 +4,60 @@
 
 gcc -o ft_unset ./main.c ./../../unset.c  ./../../env.c -L../../../42_MyLibC -lft
 
-env | sort > original_env.txt
+# env | sort > original_env.txt
 # cat original_env.txt | sort > cpy_original_env.txt
 
 # Lire le fichier d'entrée et définir les variables d'environnement avec export
-while IFS=' ' read -r export_val unset_val; do
-    export $export_val && env > env_update.txt
+while IFS=' ' read -r export_var unset_val; do
+    export $export_var
 done < variables.txt
-# cat env_update.txt
+#  cat env_update.txt
 
-# Lire le fichier d'entrée et définir les variables d'environnement avec export
+# Lire le fichier d'entrée et définir les variables d'environnement avec unset
 while IFS=' ' read -r export_val unset_val; do
     unset $unset_val && env > env_unset.txt
 done < variables.txt
 # cat env_unset.txt
 
 # Comparer les variables d'environnement avant et après l'exécution de unset
-diff env_update.txt env_unset.txt
-
-# # Créer un fichier temporaire pour stocker les résultats
-temp_file=$(mktemp)
-
+# diff env_update.txt env_unset.txt
 
 while IFS=' ' read -r export_val unset_val; do
+    export $export_val && env > env_update.txt
+done < variables.txt
+# cat env_update.txt
 
+
+
+# Lire le fichier dentree et utiliser la fonction ft_unset
+# si ft unset sest bien effectue mettre a jour env
+# sinon ecrire un message derreur
+while IFS=' ' read -r export_val unset_val; do
+	./ft_unset $unset_val | sort > ft_tmp.txt
 	# Rechercher la variable dans le fichier
-	if grep -q "^$variable=" "$fichier"; then
+	if grep -q "^$unset_val=" ft_tmp.txt; then
 		# Afficher la variable et sa valeur
-		grep "^$variable=" "$fichier" >> env_unset.txt
+		echo "ERR:var '$unset_val', NOT unset"
+	else
+		# Mettre a jour les variables d'environnement pour la prochaine iteration
+		unset $unset_val && env > ft_update.txt
 	fi
+done < variables.txt
 
+# Trier avec sort tout les fichier txt
+sort env_update.txt > env_update_sort.txt
+sort ft_update.txt > ft_update_sort.txt
+sort ft_tmp.txt > ft_tmp_sort.txt
 
+# # Comparer les varaiables d'environnement avant et apres executer ft_unset
+diff ft_tmp_sort.txt ft_update_sort.txt
 
-# # Créer un fichier temporaire pour stocker les résultats
-# temp_file=$(mktemp)
-
-# # Lire chaque ligne du fichier original_file
-# while IFS= read -r export_val unset_val; do
-#     # Appliquer le programme pgrm sur la ligne
-#     result=$(echo "$line" | ./pgrm unset_val)
-
-#     # Ajouter le résultat au fichier temporaire
-#     echo "$result" >> "$temp_file"
-# done < original_file
-
-# # Remplacer le fichier original_file par le fichier temporaire
-# mv "$temp_file" original_file
 
 # # Comparer les variables d'environnement
 # diff original_env.txt custom_env.txt
 
-# # Nettoyer les fichiers temporaires
-# rm original_env.txt custom_env.txt new_env.txt ft_unset
+# # Nettoyer les fichiers temporaires txt non tries
+rm env_update.txt ft_update.txt ft_tmp.txt env_unset.txt
+
+# # Nettoyer les fichiers temporaires txt tries
+rm env_update_sort.txt ft_update_sort.txt ft_tmp_sort.txt
