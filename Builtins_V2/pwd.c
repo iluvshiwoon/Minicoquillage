562270a55ddc41239e8dbb3d4ec3f7183967ae88
iluvshiwoon <sntcillian@gmail.com>
2024-11-16 03:22:46 +0000

    update pwd

--- Builtins_V2/pwd.c
+++ Builtins_V2/pwd.c
@@ -3,13 +3,19 @@
 int	mini_pwd(void)
 {
 	char	*cwd;
+    size_t w_bytes;
+    int r_value;
 
 	cwd = getcwd(NULL, 0);
+    w_bytes = 0;
+    r_value = 0;
 	if (cwd)
 	{
-		printf("%s\n", cwd);
+        w_bytes = write(1, cwd, ft_strlen(cwd));
+        w_bytes += write(1, "\n", 1);
+        if (w_bytes != ft_strlen(cwd) + 1)
+            r_value = 1;
 		free(cwd);
-		return (0);
 	}
-	return (1);
+	return (r_value);
 }
