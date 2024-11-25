5d38413bcb398be43437ed82f68ae8522e87def9
iluvshiwoon <sntcillian@gmail.com>
2024-11-25 11:47:08 +0000

    done heredoc

--- Exec/heredoc.c
+++ Exec/heredoc.c
@@ -6,11 +6,96 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 17:32:45 by kgriset           #+#    #+#             */
-/*   Updated: 2024/11/25 01:31:58 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/11/25 11:42:53 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Minicoquillage.h"
+
+void here_assign(char * to_expand, char * expanded)
+{
+    int j;
+    int l;
+    t_open_quote open;
+
+    open.double_quote = 0;
+    open.single_quote = 0;
+    j = -1;
+    l = -1;
+    while (to_expand[++j])
+    {
+        if (_handle_quote(to_expand[j], &open) == EXIT_SUCCESS);
+        else if (to_expand[j] == '"' && open.double_quote)
+            open.double_quote = 0;
+        else if (to_expand[j] == '\'' && open.single_quote)
+            open.single_quote = 0;
+        else
+            expanded[++l] = to_expand[j];
+        if (!to_expand[j])
+            break;
+    }
+}
+
+int here_count(char * str)
+{
+    int i;
+    int count;
+    t_open_quote open;
+
+    i = -1;
+    count = 0;
+    open.double_quote = 0;
+    open.single_quote = 0;
+    while(str[++i])
+    {
+        if (_handle_quote(str[i], &open) == EXIT_SUCCESS);
+        else if (str[i] == '"' && open.double_quote)
+            open.double_quote = 0;
+        else if (str[i] == '\'' && open.single_quote)
+            open.single_quote = 0;
+        else
+            ++count;
+        if (!str[i])
+            break;
+    }
+    return count;
+}
+
+bool here_is_empty_quote(char * to_expand)
+{
+    if (to_expand[0] == '\'' || to_expand[0] == '"')
+    {
+        if (to_expand[1] == to_expand[0])
+            return true;
+    }    
+    return false;
+}
+
+char * _quote(t_heap * heap, char * to_expand)
+{
+    char * expanded;
+    int i;
+    int count;
+
+    i = -1;
+    expanded = NULL;
+    while (to_expand[++i])
+    {
+        count = here_count(to_expand);
+        if (!count && here_is_empty_quote(to_expand))
+        {
+            expanded = wrap_malloc(heap->heap_allocated, heap->list, sizeof(char));
+            expanded[0] = '\0';
+        }
+        else if (count)
+        {
+            expanded = wrap_malloc(heap->heap_allocated, heap->list, sizeof(char) * (count + 1));
+            expanded[count] = '\0';
+            here_assign(to_expand,expanded);
+        }
+    }
+    return (expanded);
+}
 
 void mini_write(t_heap * heap,int fd, char * str, ssize_t bytes)
 {
@@ -71,6 +156,7 @@ void    listen_heredoc(t_heap * heap, int * fd, char * eof)
     //     eof = mini_ft_strtrim(heap,eof,"\"");
     // else if (eof[0] == '\'')
     //     eof = mini_ft_strtrim(heap,eof,"\'");
+    eof = _quote(heap, eof);
     while (g_signal != SIGINT)
     {
         ++i;
@@ -106,17 +192,15 @@ void    listen_heredoc(t_heap * heap, int * fd, char * eof)
 void    run_heredoc(t_heap * heap, t_atom * atom)
 {
     int i;
-    t_expanded * expanded;
 
     i = -1;
-    expanded = _expand(heap, atom->heredoc_eof, NULL, 0);
-    while (expanded->value[++i])
+    while (atom->heredoc_eof[++i])
     {
         if (g_signal == SIGINT)
             break;
-        if (atom->heredoc && expanded->value[i+1] == NULL)
+        if (atom->heredoc && atom->heredoc_eof[i+1] == NULL)
             open_heredoc(heap, atom);
-        listen_heredoc(heap,&atom->in_fd,expanded->value[i]);
+        listen_heredoc(heap,&atom->in_fd,atom->heredoc_eof[i]);
     }
 }
 
--- Expand/expand.h
+++ Expand/expand.h
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 16:29:12 by kgriset           #+#    #+#             */
-/*   Updated: 2024/11/16 05:00:56 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/11/25 11:27:59 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,5 @@ t_expanded * _expand(t_heap * heap, char ** to_expand, char ** envp, int status)
 char ** _glob_args(t_heap * heap, t_expanded * expanded);
 int _count_glob(t_heap * heap, char * str, bool * litteral);
 char * _getenv(t_heap * heap, char * var, char ** envp, int status);
+int	_handle_quote(char c, t_open_quote *open);
 #endif
