195554a10087eed443c378102830cc8cd6eb412d
iluvshiwoon <sntcillian@gmail.com>
2024-11-26 10:36:48 +0100

    fix env -i

--- Minicoquillage.c
+++ Minicoquillage.c
@@ -6,7 +6,7 @@
 /*   By: kgriset <kgriset@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 17:17:20 by kgriset           #+#    #+#             */
-/*   Updated: 2024/11/26 10:32:12 by kgriset          ###   ########.fr       */
+/*   Updated: 2024/11/26 10:35:57 by kgriset          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,7 +74,7 @@ int	main(int argc, char **argv, char ** envp)
     sa.sa_flags = 0;
     sigaction(SIGINT, &sa, NULL);
     mini.envp = envp;
-    if (!envp)
+    if (!envp || !envp[0])
         return(1);
     if (MODE == INTERACTIVE && isatty(STDIN_FILENO))
     {
